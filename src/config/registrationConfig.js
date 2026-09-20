e7272ba1ca73b9a7fc76439e0b0a4cd5ab50a261
beckyherve6848 <beckyherve@gmail.com>
2026-09-20 17:49:12 +0300

    Update translations and other changes

--- src/config/registrationConfig.js
+++ src/config/registrationConfig.js
@@ -30,15 +30,79 @@ export const config = {
     // ---- Overlay opacity for readability (0-100) ----
     overlayOpacity: 65,
 
-    // ---- Provinces & their communes (Bujumbura region) ----
+    // ---- All 18 Provinces of Burundi & their communes ----
     provinces: [
         {
+            name: "Bubanza",
+            communes: ["Bubanza", "Gihanga", "Mpanda", "Musigati", "Rugazi"],
+        },
+        {
             name: "Bujumbura Mairie",
-            communes: ["Mukaza", "Muha", "Ntahangwa"],
+            communes: ["Muha", "Mukaza", "Ntahangwa"],
         },
         {
             name: "Bujumbura Rural",
-            communes: ["Kabezi", "Isale", "Mubimbi", "Mutambu", "Mugongo-Manga"],
+            communes: ["Isale", "Kabezi", "Kanyosha", "Mubimbi", "Mugongomanga", "Mukike", "Mutambu", "Mutimbuzi", "Nyabiraba"],
+        },
+        {
+            name: "Bururi",
+            communes: ["Bururi", "Matana", "Mugamba", "Rutovu", "Songa", "Vyanda"],
+        },
+        {
+            name: "Cankuzo",
+            communes: ["Cankuzo", "Cendajuru", "Gisagara", "Kigamba", "Mishiha"],
+        },
+        {
+            name: "Cibitoke",
+            communes: ["Buganda", "Bukinanyana", "Mabayi", "Mugina", "Murwi", "Rugombo"],
+        },
+        {
+            name: "Gitega",
+            communes: ["Bugendana", "Bukirasazi", "Buraza", "Giheta", "Gishubi", "Gitega", "Itaba", "Makebuko", "Mutaho", "Nyanrusange", "Ryansoro"],
+        },
+        {
+            name: "Karuzi",
+            communes: ["Buhiga", "Gihogazi", "Gitaramwe", "Mutumba", "Nyabikere", "Shombo"],
+        },
+        {
+            name: "Kayanza",
+            communes: ["Butaganzwa", "Gahombo", "Gatara", "Kabarore", "Kayanza", "Matongo", "Muhanga", "Muruta", "Rango"],
+        },
+        {
+            name: "Kirundo",
+            communes: ["Bugabira", "Busoni", "Bwambarangwe", "Gitobe", "Kirundo", "Ntega", "Vumbi"],
+        },
+        {
+            name: "Makamba",
+            communes: ["Kayogoro", "Kibago", "Mabanda", "Makamba", "Nyanza-Lac", "Vugizo"],
+        },
+        {
+            name: "Muramvya",
+            communes: ["Bukeye", "Kiganda", "Mbuye", "Muramvya", "Rutegama"],
+        },
+        {
+            name: "Muyinga",
+            communes: ["Buhinyuza", "Butihinda", "Gashoho", "Gasorwe", "Giteranyi", "Muyinga", "Mwakiro"],
+        },
+        {
+            name: "Mwaro",
+            communes: ["Bisoro", "Gisozi", "Kayokwe", "Ndava", "Nyabihanga", "Rusaka"],
+        },
+        {
+            name: "Ngozi",
+            communes: ["Busiga", "Gashikanwa", "Kiremba", "Marangara", "Mwumba", "Ngozi", "Nyamurenza", "Ruhororo", "Tangara"],
+        },
+        {
+            name: "Rumonge",
+            communes: ["Bugarama", "Burambi", "Buyengero", "Muhuta", "Rumonge"],
+        },
+        {
+            name: "Rutana",
+            communes: ["Bukemba", "Giharo", "Gitanga", "Mpinga-Kayove", "Musongati", "Rutana"],
+        },
+        {
+            name: "Ruyigi",
+            communes: ["Butaganzwa", "Butezi", "Bweru", "Gisuru", "Kinyinya", "Nyabitsinda", "Ruyigi"],
         },
     ],
 };
